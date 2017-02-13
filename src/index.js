363210fbf75417785331aa933ae15c1e8a11f570
Leonid Logvinov <logvinov.leon@gmail.com>
2017-02-13 16:26:26 +0100

    Add documentation

--- src/index.js
+++ src/index.js
@@ -16,7 +16,15 @@ function randomShuffle(array) {
     return array;
 }
 
+/**
+ * Redundant RPC provider
+ */
 class RedundantRPCSubprovider extends Subprovider {
+    /**
+     * Create a provider
+     * @param {Array.<string>} endpoints - RPC endpoints.
+     * @param {Subprovider} RpcSubprovider - RPC Subprovider
+     */
     constructor(endpoints, RpcSubprovider) {
         super(endpoints, RpcSubprovider);
         this.RPCs = endpoints.map((endpoint)=> {
@@ -26,21 +34,37 @@ class RedundantRPCSubprovider extends Subprovider {
         });
     }
 
+    /**
+     * Handles RPC request
+     * @param payload - payload
+     * @param next
+     * @param end - callback
+     */
     handleRequest(payload, next, end) {
         if (payload.method === "eth_sendRawTransaction") {
+            // We want to send transactions to all to speed up propagation and omit censoring
             this._multiCastFirstSuccess(payload, next, end);
         } else {
+            // Randomly shuffle RPCs to balance the load
             let RPCs = randomShuffle(this.RPCs.slice());
             this._uniCastFirstSuccess(RPCs, payload, next, end);
         }
     }
 
+    /**
+     * Returns callback for multiple use
+     * which resolves on first success, or fails with last error
+     * @param callback
+     * @return {function(*=, *=)}
+     * @private
+     */
     _firstSuccess(callback) {
-        let tries = 0;
-        let succeeded = false;
+        let tries = 0; // Number of tries
+        let succeeded = false; // Did we get any successful response
         return (err, data)=> {
             if (err) {
                 ++tries;
+                // All RPCs failed
                 if (tries === this.RPCs.length) callback(err);
             } else {
                 if (!succeeded) {
@@ -51,21 +75,39 @@ class RedundantRPCSubprovider extends Subprovider {
         }
     }
 
+    /**
+     * Sends to all RPCs and waits for first success
+     * @param payload - payload
+     * @param next
+     * @param end - callback
+     * @private
+     */
     _multiCastFirstSuccess(payload, next, end) {
         let cb = this._firstSuccess(end);
         this.RPCs.map((RPC)=>RPC.handleRequest(payload, next, cb))
     }
 
+    /**
+     * Sends to a single RPC, but uses consequent ones if it fails
+     * @param RPCs - list of RPCs
+     * @param payload - payload
+     * @param next
+     * @param end - callback
+     * @private
+     */
     _uniCastFirstSuccess(RPCs, payload, next, end) {
         let RPC = RPCs.shift();
         RPC.handleRequest(payload, next, (err, data)=> {
             if (err) {
                 if (RPCs.length !== 0) {
+                    // Try next RPC
                     this._uniCastFirstSuccess(RPCs, payload, next, end)
                 } else {
+                    // All RPCs failed
                     end(err);
                 }
             } else {
+                // Success
                 end(undefined, data);
             }
         })
